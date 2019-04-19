0270d7fdda832dde18c4f6947a13b213121e22ae
Micah J. Martin <mjm5097@rit.edu>
2019-04-18 20:48:12 -0400

    Change env vars

--- TheArk.py
+++ TheArk.py
@@ -28,8 +28,13 @@ def cleanup():
 
 if __name__ == "__main__":
     setup()
-    host = os.environ.get("ARK_LISTEN_IP", "0.0.0.0")
-    port = os.environ.get("ARK_LISTEN_PORT", "5000")
-
-    app.run(host=host, port=port, debug=True)
+    host = os.environ.get("FLASK_HOST", "0.0.0.0")
+    try:
+        port = os.environ.get("FLASK_PORT", "5000")
+        port = int(port)
+    except ValueError:
+        port = 5000
+    debug = os.environ.get("FLASK_DEBUG", "True")
+    debug = debug.lower().strip() in ["true", "yes", "1", "t"]
+    app.run(debug=debug, host=host, port=port)
     cleanup()
